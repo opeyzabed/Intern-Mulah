739682de78ab1b347c8623b5104e754ba0cb4c63
opeyzabed <103284320+opeyzabed@users.noreply.github.com>
2023-07-28 01:47:51 +0800

    baiki

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
+    
+
     // Function to read and display CSV data in HTML table
     function displayCSVData(data) {
         // Split the CSV data into an array of lines
         var lines = data.split("\n");
 
-        // Initialize an array to hold the table data
-        var tableData = [];
-
+        var row = {};
+        
         // Loop through each line and convert it into an array of values
         for (var i = 0; i < lines.length; i++) {
             var values = lines[i].split(",");
@@ -16,27 +17,34 @@ $(document).ready(function() {
                 // Assuming the first row is the header row
                 // Create the table header row
                 $("#csvTable thead").append("<tr></tr>");
-                for (var j = 0; j < values.length; j++) {
-                    $("#csvTable thead tr").append("<th>" + values[j] + "</th>");
-                }
+                $("#csvTable thead tr").append("<th>" + values[0] + "</th>");
+                $("#csvTable thead tr").append("<th>" + values[1] + "</th>");
             } else {
                 // Create an object representing a row in the table
-                var row = {};
-                for (var k = 0; k < values.length; k++) {
-                    row["column_" + (k + 1)] = values[k];
-                }
-                // Add the row object to the tableData array
-                tableData.push(row);
+                
+                row[values[0]] = parseInt(values[1]);
+
+                $("#csvTable tbody").append("<tr></tr>");
+                $("#csvTable tbody tr:last-child").append("<td>" + values[0] + "</td>");
+                $("#csvTable tbody tr:last-child").append("<td>" + values[1] + "</td>");               
             }
         }
 
-        // Loop through the tableData array and create the table rows
-        for (var l = 0; l < tableData.length; l++) {
-            $("#csvTable tbody").append("<tr></tr>");
-            for (var prop in tableData[l]) {
-                $("#csvTable tbody tr:last-child").append("<td>" + tableData[l][prop] + "</td>");
-            }
-        }
+        displayCategory(row);
+    }
+
+    function displayCategory(data){
+        $("#table2 tbody").append("<tr></tr>");
+        $("#table2 tbody tr:last-child").append("<td>" + "Alpha" + "</td>");
+        $("#table2 tbody tr:last-child").append("<td>" + (data.A5+data.A20) + "</td>"); 
+
+        $("#table2 tbody").append("<tr></tr>");
+        $("#table2 tbody tr:last-child").append("<td>" + "Beta" + "</td>");
+        $("#table2 tbody tr:last-child").append("<td>" + (data.A15/data.A7) + "</td>"); 
+
+        $("#table2 tbody").append("<tr></tr>");
+        $("#table2 tbody tr:last-child").append("<td>" + "Charlie" + "</td>");
+        $("#table2 tbody tr:last-child").append("<td>" + (data.A13*data.A12) + "</td>"); 
     }
 
     // Read the CSV file using jQuery AJAX
